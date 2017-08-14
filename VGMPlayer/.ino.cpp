806ef72df3898de05f2533ca8dd76063a1674de0
Philipp König <kingcastle@gmx.net>
2017-08-14 18:14:47 +0200

    rename

--- VGMPlayer/.ino.cpp
+++ VGMPlayer/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-08-14 18:12:07
+//This file has been generated on 2017-08-14 18:14:22
 
 #include "Arduino.h"
 #include "VGMPlayer.h"
